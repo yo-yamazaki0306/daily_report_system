573981bfd30a96b203e6c86449a85af2a5774f50
you.yamazaki <youyou0306@gmail.com>
2023-01-24 19:17:33 +0900

    従業員createアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -9,6 +9,8 @@ import actions.views.EmployeeView;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.JpaConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.EmployeeService;
 
 /**
@@ -85,5 +87,58 @@ public class EmployeeAction extends ActionBase {
         forward(ForwardConst.FW_EMP_NEW);
 
     }
+
+    /**
+     * 新規登録を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+
+    public void create() throws ServletException,IOException{
+
+        //CSRF対策 tokenのチェック
+        if(checkToken()) {
+
+            //パラメータの値を元に従業員情報のインスタンスを作成する
+            EmployeeView ev = new EmployeeView(
+                    null,
+                    getRequestParam(AttributeConst.EMP_CODE),
+                    getRequestParam(AttributeConst.EMP_NAME),
+                    getRequestParam(AttributeConst.EMP_PASS),
+                    toNumber(getRequestParam(AttributeConst.EMP_ADMIN_FLG)),
+                    null,
+                    null,
+                    AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
+
+            //アプリケーションスコープからpepper文字列を取得
+            String pepper = getContextScope(PropertyConst.PEPPER);
+
+            //従業員情報登録
+            List<String> errors = service.create(ev, pepper);
+
+            if(errors.size()>0) {
+                //登録中にエラーがあった場合
+
+                putRequestScope(AttributeConst.TOKEN,getTokenId()); //CSRF対策曜トークン
+                putRequestScope(AttributeConst.EMPLOYEE,ev);//入力された従業員情報
+                putRequestScope(AttributeConst.ERR,errors); //エラーのリスト
+
+                //新規登録画面を再表示
+                forward(ForwardConst.FW_EMP_NEW);
+
+            }else {
+
+                //登録中にエラーがなかった場合
+
+                //セッションに登録完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
+
+                //一覧表画面にリダイレクト
+                redirect(ForwardConst.ACT_EMP,ForwardConst.CMD_INDEX);
+            }
+
+        }
+    }
+
 
 }
