8f989c4c8cbd0343f12edf417b4be39916dcc8a1
you.yamazaki <youyou0306@gmail.com>
2023-01-26 20:37:28 +0900

    日報createアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -1,16 +1,18 @@
 package actions;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.JpaConst;
+import constants.MessageConst;
 import services.ReportService;
-
 /**
  * 日報に関する処理を行うActionクラス
  *
@@ -68,4 +70,81 @@ public class ReportAction extends ActionBase {
         forward(ForwardConst.FW_REP_INDEX);
 
     }
+
+    /**
+     * 新規登録画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void entryNew() throws ServletException, IOException {
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+
+        //日報情報の空インスタンスに、日報の日付＝今日の日付を設定する
+        ReportView rv = new ReportView();
+        rv.setReportDate(LocalDate.now());
+        putRequestScope(AttributeConst.REPORT, rv); //日付のみ設定済みの日報インスタンス
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_REP_NEW);
+
+    }
+
+    /**
+    * 新規登録を行う
+    * @throws ServletException
+    * @throws IOException
+    */
+   public void create() throws ServletException, IOException {
+
+       //CSRF対策 tokenのチェック
+       if (checkToken()) {
+
+           //日報の日付が入力されていなければ、今日の日付を設定
+           LocalDate day = null;
+           if (getRequestParam(AttributeConst.REP_DATE) == null
+                   || getRequestParam(AttributeConst.REP_DATE).equals("")) {
+               day = LocalDate.now();
+           } else {
+               day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
+           }
+
+           //セッションからログイン中の従業員情報を取得
+           EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+           //パラメータの値をもとに日報情報のインスタンスを作成する
+           ReportView rv = new ReportView(
+                   null,
+                   ev, //ログインしている従業員を、日報作成者として登録する
+                   day,
+                   getRequestParam(AttributeConst.REP_TITLE),
+                   getRequestParam(AttributeConst.REP_CONTENT),
+                   null,
+                   null);
+
+           //日報情報登録
+           List<String> errors = service.create(rv);
+
+           if (errors.size() > 0) {
+               //登録中にエラーがあった場合
+
+               putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+               putRequestScope(AttributeConst.REPORT, rv);//入力された日報情報
+               putRequestScope(AttributeConst.ERR, errors);//エラーのリスト
+
+               //新規登録画面を再表示
+               forward(ForwardConst.FW_REP_NEW);
+
+           } else {
+               //登録中にエラーがなかった場合
+
+               //セッションに登録完了のフラッシュメッセージを設定
+               putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
+
+               //一覧画面にリダイレクト
+               redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
+           }
+       }
+   }
+
 }
